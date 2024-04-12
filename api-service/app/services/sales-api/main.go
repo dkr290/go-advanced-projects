ca4bf664bb4b4a9fa061bc943f34ce646079bd6b
Danail <d_krasimirov@yahoo.com>
2024-04-12 22:53:16 +0200

    some logging

--- api-service/app/services/sales-api/main.go
+++ api-service/app/services/sales-api/main.go
@@ -1,7 +1,43 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
 
+	"github.com/dkr290/go-advanced-projects/api-service/foundation/logger"
+	"go.uber.org/zap"
+)
+
+func main() {
+
+	log, err := logger.New("SALES-API")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer log.Sync()
+
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", err)
+		log.Sync()
+		os.Exit(1)
+	}
 
 }
 
+func run(log *zap.SugaredLogger) error {
+	// GOMAXPROX
+	log.Infow("startup", "GOMAXPROX", runtime.GOMAXPROCS(1))
+	//--------------------------------------------------------------
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-shutdown
+
+	log.Infow("shutdown", "status", "shutdown started", "signal", sig)
+	defer log.Infow("shutdown", "status", "shutdown complete", "signal", sig)
+
+	return nil
+}
