0ac0c0451b72a52789b482bb69dea6278ccb25a3
Danail <d_krasimirov@yahoo.com>
2024-06-19 22:53:45 +0200

    adding some models for factory pattern

---
+++ design-pattern/go-app/models/models.go
@@ -0,0 +1,33 @@
+package models
+
+type DogBreed struct {
+	ID               int    `json:"id"`
+	Breed            string `json:"breed"`
+	WeightLowLbs     int    `json:"weight_low_lbs"`
+	WeightHighLbs    int    `json:"weight_high_lbs"`
+	AverageWeight    int    `json:"average_weight"`
+	Lifespan         int    `json:"average_lifespan"`
+	Details          string `json:"details"`
+	AlternateNames   string `json:"alternate_names"`
+	GeorgaphicOrigin string `json:"geographic_origin"`
+}
+
+type CatBreed struct {
+	ID               int    `json:"id"`
+	Breed            string `json:"breed"`
+	WeightLowLbs     int    `json:"weight_low_lbs"`
+	WeightHighLbs    int    `json:"weight_high_lbs"`
+	AverageWeight    int    `json:"average_weight"`
+	Lifespan         int    `json:"average_lifespan"`
+	Details          string `json:"details"`
+	AlternateNames   string `json:"alternate_names"`
+	GeorgaphicOrigin string `json:"geographic_origin"`
+}
+
+type Dog struct{}
+
+type Cat struct{}
+
+type Breeder struct{}
+
+type Pet struct{}
