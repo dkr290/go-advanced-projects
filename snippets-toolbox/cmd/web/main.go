387aaddc00b56f578528edef1097f0d4c3dc2213
Danail <d_krasimirov@yahoo.com>
2024-07-30 22:47:08 +0200

    adding some command line params

--- snippets-toolbox/cmd/web/main.go
+++ snippets-toolbox/cmd/web/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
-const PORT = ":4000"
-
 func main() {
+
+	var cfg config
+
+	//define a new command-line flag with the name addr and default value
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	// Importantly, we use the flag.Parse() function to parse the command-line flag.
+	// This reads in the command-line flag value and assigns it to the addr
+	// variable. You need to call this *before* you use the addr variable
+	// otherwise it will always contain the default value of ":4000". If any
+	// errors are
+	// encountered during parsing the application will be terminated.
+
+	flag.Parse()
 
 	mux := http.NewServeMux()
 	//create a file server which serves files out of "./ui/static direct all "
@@ -19,9 +31,13 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
+	// The value returned from the flag.String() function is a pointer to theflag
+	// value, not the value itself. So we need to dereference the pointer
+	// prefix it with the * symbol) before using it. Note that it is using
+	// log.Printf() function to interpolate the address with the log message.
 
-	log.Print("Starting server on", PORT)
-	if err := http.ListenAndServe(PORT, mux); err != nil {
+	log.Printf("Starting server on %s", cfg.addr)
+	if err := http.ListenAndServe(cfg.addr, mux); err != nil {
 
 		log.Fatal(err)
 	}
