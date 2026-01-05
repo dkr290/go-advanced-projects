e43c6fab1e7a63f37c38c609ed3f6964f9cbded7
Danail Surudzhiyski <dans@bankingcircle.com>
2026-01-05 17:38:04 +0100

    add root website sequential agent

---
+++ sequential-website-builder/agents/root-website-builder/root-website-builder.go
@@ -0,0 +1,50 @@
+package rootwebsitebuilder
+
+import (
+	"fmt"
+
+	codewriter "seq-website-builder/agents/code-writer"
+	"seq-website-builder/agents/designer"
+	requirementswriter "seq-website-builder/agents/requirements-writer"
+	"seq-website-builder/utils"
+
+	"google.golang.org/adk/agent"
+	"google.golang.org/adk/agent/workflowagents/sequentialagent"
+)
+
+func SequentialAgent() (agent.Agent, error) {
+	codeWriterAgent, err := codewriter.CodeWriterAgent("APIKEY")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create code writer agent: %v", err)
+	}
+
+	designerAgent, err := designer.Designer("APIKEY")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create code writer agent: %v", err)
+	}
+	requrenmentsWriterAgent, err := requirementswriter.Writer("apikey")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create code writer agent: %v", err)
+	}
+
+	desc, err := utils.LoadInstructionsFile("./description.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	codePipelineAgent, err := sequentialagent.New(sequentialagent.Config{
+		AgentConfig: agent.Config{
+			Name:        "root_website_builder_agent",
+			Description: desc,
+			SubAgents: []agent.Agent{
+				requrenmentsWriterAgent,
+				designerAgent,
+				codeWriterAgent,
+			},
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create sequential agent: %v", err)
+	}
+	return codePipelineAgent, nil
+}
