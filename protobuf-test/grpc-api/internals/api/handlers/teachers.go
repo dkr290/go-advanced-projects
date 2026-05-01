10d3d49d2f6c5e88033b7006c281a563498400c2
dani <d_krasimirov@yahoo>
2026-05-01 14:26:33 +0200

    add some tests

--- protobuf-test/grpc-api/internals/api/handlers/teachers.go
+++ protobuf-test/grpc-api/internals/api/handlers/teachers.go
@@ -2,7 +2,9 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/dkr290-go-advanced-projects/protobuf-test/grpc-api/models"
 	pb "github.com/dkr290-go-advanced-projects/protobuf-test/grpc-api/proto/gen"
 	"github.com/dkr290-go-advanced-projects/protobuf-test/grpc-api/repositories/mongodb"
 )
@@ -13,4 +15,30 @@ func (s *Server) AddTeachers(ctx context.Context, req *pb.Teachers) (*pb.Teacher
 		return nil, s.Log.Errorf("Error mongodb connection %v", err)
 	}
 	defer client.Disconnect(ctx)
+
+	newTeachers := make([]*models.Teacher, len(req.GetTeachers()))
+
+	for i, pbTeacher := range req.GetTeachers() {
+		mt := models.Teacher{
+			FirstName: pbTeacher.FirstName,
+			LastName:  pbTeacher.LastName,
+			Email:     pbTeacher.Email,
+			Class:     pbTeacher.Class,
+			Subject:   pbTeacher.Subject,
+		}
+
+		newTeachers[i] = &mt
+		fmt.Println(mt)
+	}
+	// // Insert into MongoDB (adjust database/collection names)
+	// collection := client.Database("school").Collection("teachers")
+	// insertResult, err := collection.InsertMany(ctx, newTeachers)
+	// if err != nil {
+	// 	return nil, s.Log.Errorf("Error inserting teachers: %v", err)
+	// }
+	//
+	// // Optionally update IDs in the response
+	// // for i, id := range insertResult.InsertedIDs { ... }
+	//
+	return nil, nil
 }
---
+++ protobuf-test/grpc-api/models/models.go
@@ -0,0 +1,11 @@
+// Package models for the model structures of the mongodb messagess
+package models
+
+type Teacher struct {
+	ID        string `protobuf:"id,omitempty"         bson:"_id,omitempty"`
+	FirstName string `protobuf:"first_name,omitempty" bson:"first_name,omitempty"`
+	LastName  string `protobuf:"last_name,omitempty"  bson:"last_name,omitempty"`
+	Email     string `protobuf:"email,omitempty"      bson:"email,omitempty"`
+	Class     string `protobuf:"class,omitempty"      bson:"class,omitempty"`
+	Subject   string `protobuf:"subject,omitempty"    bson:"subject,omitempty"`
+}
